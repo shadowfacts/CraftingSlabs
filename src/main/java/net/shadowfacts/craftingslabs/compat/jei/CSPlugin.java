adafa9fbe43a7293458f6b342ae51988a950662d
Shadowfacts <me@shadowfacts.net>
2016-01-14 20:59:54 -0500

    JEI compat

---
+++ src/main/java/net/shadowfacts/craftingslabs/compat/jei/CSPlugin.java
@@ -0,0 +1,39 @@
+package net.shadowfacts.craftingslabs.compat.jei;
+
+import mezz.jei.api.*;
+import mezz.jei.api.recipe.VanillaRecipeCategoryUid;
+import mezz.jei.api.recipe.transfer.IRecipeTransferRegistry;
+import net.shadowfacts.craftingslabs.container.ContainerCrafting;
+
+/**
+ * @author shadowfacts
+ */
+@JEIPlugin
+public class CSPlugin implements IModPlugin {
+
+	private IItemRegistry itemRegistry;
+	private IJeiHelpers jeiHelpers;
+
+	@Override
+	public void onJeiHelpersAvailable(IJeiHelpers jeiHelpers) {
+		this.jeiHelpers = jeiHelpers;
+	}
+
+	@Override
+	public void onItemRegistryAvailable(IItemRegistry itemRegistry) {
+		this.itemRegistry = itemRegistry;
+	}
+
+	@Override
+	public void register(IModRegistry registry) {
+		IRecipeTransferRegistry recipeTransferRegistry = registry.getRecipeTransferRegistry();
+
+		recipeTransferRegistry.addRecipeTransferHandler(ContainerCrafting.class, VanillaRecipeCategoryUid.CRAFTING, 1, 9, 10, 36);
+	}
+
+	@Override
+	public void onRecipeRegistryAvailable(IRecipeRegistry recipeRegistry) {
+
+	}
+
+}
