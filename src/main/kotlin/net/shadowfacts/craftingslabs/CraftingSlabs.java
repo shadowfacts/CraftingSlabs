5237a01d5e7e5cceab3f0daf64edf6bfdc0b6ece
Shadowfacts <me@shadowfacts.net>
2016-12-23 13:26:26 -0500

    Add item models

--- src/main/kotlin/net/shadowfacts/craftingslabs/CraftingSlabs.java
+++ src/main/kotlin/net/shadowfacts/craftingslabs/CraftingSlabs.java
@@ -1,12 +1,17 @@
 package net.shadowfacts.craftingslabs;
 
+import net.minecraft.client.renderer.block.model.ModelResourceLocation;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemBlock;
+import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.client.model.ModelLoader;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.network.NetworkRegistry;
 import net.minecraftforge.fml.common.network.simpleimpl.SimpleNetworkWrapper;
 import net.minecraftforge.fml.common.registry.GameRegistry;
 import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 import net.shadowfacts.craftingslabs.block.BlockCraftingSlab;
 import net.shadowfacts.craftingslabs.block.BlockFurnaceSlab;
 import net.shadowfacts.craftingslabs.gui.GUIHandler;
@@ -46,6 +51,13 @@ public class CraftingSlabs {
 		NetworkRegistry.INSTANCE.registerGuiHandler(this, new GUIHandler());
 		network = NetworkRegistry.INSTANCE.newSimpleChannel(MOD_ID);
 		network.registerMessage(PacketUpdateFurnaceSlab.class, PacketUpdateFurnaceSlab.class, 0, Side.CLIENT);
+	}
+
+	@Mod.EventHandler
+	@SideOnly(Side.CLIENT)
+	public void preInitClient(FMLPreInitializationEvent event) {
+		ModelLoader.setCustomModelResourceLocation(Item.getItemFromBlock(craftingSlab), 0, new ModelResourceLocation(new ResourceLocation(MOD_ID, "crafting_slab"), "inventory"));
+		ModelLoader.setCustomModelResourceLocation(Item.getItemFromBlock(furnaceSlab), 0, new ModelResourceLocation(new ResourceLocation(MOD_ID, "furnace_slab"), "inventory"));
 	}
 
 }
